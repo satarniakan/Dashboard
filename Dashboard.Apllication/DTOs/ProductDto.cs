d6b312f913aca07fe1b08c1c39c947809a2c317c
satarniakan <satar_niakan@yahoo.com>
2026-09-04 01:50:24 -0700

    edit product
    edit product

--- Dashboard.Apllication/DTOs/ProductDto.cs
+++ Dashboard.Apllication/DTOs/ProductDto.cs
@@ -1,10 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿// Dashboard.Application/DTOs/ProductDto.cs
+using System.ComponentModel.DataAnnotations;
 
 namespace Dashboard.Application.DTOs;
 
 public record ProductDto(int Id, string Name, decimal Price);
 
 public class CreateProductDto
+{
+    [Required(ErrorMessage = "نام الزامی است.")]
+    [StringLength(100, ErrorMessage = "نام نمی‌تواند بیشتر از ۱۰۰ کاراکتر باشد.")]
+    public string Name { get; set; } = string.Empty;
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "قیمت باید بزرگتر از صفر باشد.")]
+    public decimal Price { get; set; }
+}
+
+public class UpdateProductDto
 {
     [Required(ErrorMessage = "نام الزامی است.")]
     [StringLength(100, ErrorMessage = "نام نمی‌تواند بیشتر از ۱۰۰ کاراکتر باشد.")]
